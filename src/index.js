da5631e23e7623b6f88454158be5b99cbe82a2e7
chanwkkk <chanwk18@hotmail.com>
2020-03-18 12:21:36 -0600

    set up store and thunk

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import {Provider,applyMiddleware} from 'redux'
-// import {createStore} from 'redux'
-// // import {thunk} from 'redux-thunk'
+import {Provider,applyMiddleware} from 'redux'
+import {createStore} from 'redux'
+import {thunk} from 'redux-thunk'
 
-// let store = createStore(reducer, applyMiddleware(thunk))
+let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
-
+<Provider store={store}>
+    <App />
+</Provider>,
     document.getElementById('root')
 );
 
