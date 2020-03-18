87dee354e603a09620e73cddf2e9dc5f6c352466
chanwkkk <chanwk18@hotmail.com>
2020-03-18 16:47:32 -0600

    turn LoginForm into a controlled form

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,11 +1,43 @@
 import React from 'react'
 
 class LoginForm extends React.Component{
+    
+    state = {
+        email:"",
+        password: ""
+    }
+
+
+    handleChange = (e) =>{
+       this.setState({
+          [e.target.name]: e.target.value 
+       })
+    }
+
+    handleSubmit = () =>{
+
+    }
+
+
+
     render(){
-        return (
-          
+        return (        
             <div>
-               Login forrrrrrrm
+               <form>
+                   <label>Email: </label>
+                   <input 
+                      name = "email"
+                      type="text" 
+                      onChange={this.handleChange} 
+                      value={this.state.email}/>
+                   <label>Password: </label>
+                   <input 
+                      name = "password"
+                      type="password" 
+                      onChange={this.handleChange} 
+                      value={this.state.password}/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
       
