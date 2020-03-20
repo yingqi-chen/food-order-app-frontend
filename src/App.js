5868119fdd7dbc613391515e612785f8d38b5d6f
chanwkkk <chanwk18@hotmail.com>
2020-03-20 12:22:40 -0600

    add Router to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import RestaurantContainer from './containers/restaurantContainer';
 import UserContainer from './containers/userContainer.js'
 
 class App extends React.Component {
+
   
   render(){
   return (
     <div>
-      <RestaurantContainer />
-      <UserContainer />
-      {/* <FavoritesContainer />
-      <Footer> */}
+      <Router>
+       <Switch>
+         <Route exact path="/" component={RestaurantContainer} />
+       </Switch>
+      </Router>
     </div>
   );
  }
 }
 
 export default App;
+
+/* <RestaurantContainer />
+<UserContainer />
+{/* <FavoritesContainer />
+<Footer> */
