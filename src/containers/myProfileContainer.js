d7ad6f14b991b8f503bb6d9c99f1a9b2973203cf
chanwkkk <chanwk18@hotmail.com>
2020-03-28 10:53:39 -0600

    delete Favorites

--- src/containers/myProfileContainer.js
+++ src/containers/myProfileContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ListDisplayer from '../components/ListDisplayer'
 import OrdersContainer from '../components/OrdersContainer'
-import FavoritesContainer from '../components/FavoritesContainer'
 import {connect} from 'react-redux'
 
 class MyProfileContainer extends React.Component{
@@ -17,17 +16,7 @@ class MyProfileContainer extends React.Component{
     }
 
     
-    
-    
-    renderFavorites = (favorites) => {
-      return (
-        <div className="container favorite-lists">
-           <h3>My favorites</h3>
-           {this.renderListDisplayer(favorites)}
-        </div>
-      )
-    }    
-
+  
    
     render(){
         let user = this.props.user
@@ -35,9 +24,7 @@ class MyProfileContainer extends React.Component{
        <div className = "container">
          <h1>Hello, {user.name}!</h1>
          {/* <MyCurrentOrder /> */}  
-         {user.orders? <OrdersContainer orders={this.props.orders} renderListDisplayer={this.renderListDisplayer} total={this.calculateTotal}/> : <h3>You have no orders.</h3>}
-         {user.favorites? <FavoritesContainer renderListDisplayer={this.renderListDisplayer} favorites = {this.props.favorites}/>:<h3>You have no favorites.</h3>}
-  
+         {user.orders? <OrdersContainer orders={this.props.orders} renderListDisplayer={this.renderListDisplayer} total={this.calculateTotal}/> : <h3>You have no orders.</h3>}  
        </div>
    )
 }
