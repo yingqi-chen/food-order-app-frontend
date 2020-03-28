e809935bd0b18d75c3c4883a37cb8a862cc5f926
chanwkkk <chanwk18@hotmail.com>
2020-03-28 16:39:32 -0600

    add action handling to order reducer

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -5,6 +5,21 @@ const date = moment().calendar()
 const orderReducer = ( state = {dishes: [], total: 0, date: date}, action) => {
    
     switch(action.type){
+      
+
+        case "ADD_ORDER":
+            debugger
+            return{
+                ...state,
+                dishes: state.dishes.push(action.payload),
+                total: state.total + action.payload.price
+            }
+
+        case "CREATE_ORDER":
+            return state;
+        
+        case "DELETE_ORDER":
+            return state
   
           default:
               return state
