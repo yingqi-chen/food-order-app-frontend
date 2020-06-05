0975f02ad80111abdeb71b5c814cc1b0bf799c8a
chanwkkk <chanwk18@hotmail.com>
2020-06-05 00:23:28 -0600

    update fetch address to heroku address

--- src/actions/dishAction.js
+++ src/actions/dishAction.js
@@ -1,7 +1,7 @@
 function fetchDishes(dispatch){
     return dispatch =>{
         dispatch({type:"LOADING"})
-        fetch("http://localhost:3001/dishes")
+        fetch("https://powerful-oasis-63354.herokuapp.com/dishes")
         .then(r=>r.json())
         .then(j=>dispatch({type:"ADD_DISH", dishes: j}))
         .then(r=>dispatch({type: "LOADING_DONE"}))
