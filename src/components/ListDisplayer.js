e17d0de210689ce971e9fb0be394ccf3d249e6b0
chanwkkk <chanwk18@hotmail.com>
2020-06-06 14:13:42 -0600

    delete old codes

--- src/components/ListDisplayer.js
+++ src/components/ListDisplayer.js
@@ -9,9 +9,7 @@ const ListDisplayer = ({dishes,button})=>{
 
      return dishes.length!==0 ?   
             <Row>
-              {/* <Col xm ={12} sm={1} md={2} lg={3}> */}
               {dishes.map((d,index) => <Dish key={index} dish={d} button={button} />)}
-              {/* </Col> */}
             </Row>
             : null
 }
